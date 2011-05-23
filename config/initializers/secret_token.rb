ead75cd7e732aa120da2c25f68d91994ab9030d0
Al Wold <alwold@gmail.com>
2011-05-22 22:37:28 -0700

    initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classwatch::Application.config.secret_token = '6e0bf3d7c60a99d29c08546712f84288b459ffcea9be53941954f14f902251f3a07129454b72dc16863d9fa9c82dfed5d6aa4d2e012c80fff330072c4c41a838'
