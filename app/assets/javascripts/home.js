61fb3ef8b8036b0f26807a958a39cc37a8555e09
Al Wold <alwold@gmail.com>
2012-02-09 16:04:24 -0700

    revamp the home page to start matching up with the java version

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#course_course_number").keyup(function (event) {
+    if (event.target.value.length == 5) {
+      $("#spinner").show();
+      var term = $("#course_term_code").val();
+      $.ajax({url: "${lookupUrl}/1/"+term+"/"+event.target.value,
+        dataType: "json",
+        success: function(classInfo) {
+          $("#courseName").html(classInfo.name);
+          $("#spinner").hide();
+        },
+        error: function() {
+          $("#courseName").html("Error loading class title");
+          $("#spinner").hide();
+        }
+      });
+    } else {
+      $("#courseName").html("");
+    }
+  });
+  $("course_term_code").change(function (event) {
+    $("#course_course_number").val("");
+    $("#courseName").html("");
+  });
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
+    @user = User.where("email = ?", current_user.email).first
+    @courses = Course.joins(:users).where(:users => {:user_id => @user})
+    @terms = [["Spring 2012", "2121"]]
+    @notifications = Array.new
   end
 
 end
