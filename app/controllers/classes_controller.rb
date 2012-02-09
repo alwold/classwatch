eb1c1936b94fb8fe92ca8da69a8e53fa2d976591
Al Wold <alwold@gmail.com>
2012-02-09 15:04:40 -0700

    tweak to work with new models

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,15 +1,16 @@
 class ClassesController < ApplicationController
+  before_filter :authenticate_user!
   def create
     @course = Course.new(params[:course])
-    @user = User.where("email = ?", session[:user]).first
+    @user = User.where("email = ?", current_user.email).first
     @course.user = @user
     @course.save
     redirect_to :action => "index"
   end
 
   def index
-    @user = User.where("email = ?", session[:user]).first
-    @courses = Course.where("user_id = ?", @user)
+    @user = User.where("email = ?", current_user.email).first
+    @courses = Course.joins(:users).where(:users => {:user_id => @user})
   end
 
 end
