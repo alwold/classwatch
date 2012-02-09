2d7386382ec4a9944ce9e3a7148aab5331c74525
Al Wold <alwold@gmail.com>
2012-02-09 16:15:47 -0700

    - set table name so term works - add method to get current terms

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,2 +1,8 @@
+require 'date'
+
 class Term < ActiveRecord::Base
+  set_table_name "term"
+  def Term.get_active_terms
+    Term.where("start_date <= :now and end_date >= :now", { :now => Date.today })
+  end
 end
