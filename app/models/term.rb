5d8558740503a71ab79489b1081f013884b72846
Al Wold <alwold@gmail.com>
2012-02-10 14:56:12 -0700

    set primary key to match new surrogate key

--- app/models/term.rb
+++ app/models/term.rb
@@ -2,6 +2,7 @@ require 'date'
 
 class Term < ActiveRecord::Base
   set_table_name "term"
+  set_primary_key "term_id"
   def Term.get_active_terms
     Term.where("start_date <= :now and end_date >= :now", { :now => Date.today })
   end
