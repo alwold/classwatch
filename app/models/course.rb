0989896d549970d3f3bb3c7d0f84500a8966d743
Al Wold <alwold@gmail.com>
2012-02-10 14:55:44 -0700

    add association to term

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   has_many :users, :through => :user_courses
   has_many :user_courses
+  belongs_to :term
   set_table_name "course"
   set_primary_key "course_id"
 end
