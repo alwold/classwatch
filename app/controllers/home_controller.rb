6b4a49b58a262dbc80a474db761fb5d06e637e7b
Al Wold <alwold@gmail.com>
2012-02-10 14:56:33 -0700

    - show term on class listing - use term id instead of term code for select

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     @user = User.where("email = ?", current_user.email).first
     @courses = Course.joins(:user_courses => :user).where(:users => {:user_id => @user})
-    @terms = Term.get_active_terms.map { |term| [term.name, term.term_code] }
+    @terms = Term.get_active_terms.map { |term| [term.name, term.id] }
     @notifications = Array.new
   end
 
