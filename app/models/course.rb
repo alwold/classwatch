f7572ed8f8193bd7a588cf249108152b6496ee9c
Al Wold <alwold@gmail.com>
2012-02-12 19:19:14 -0700

    update table_name and primary_key stuff to use self.x

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,6 @@ class Course < ActiveRecord::Base
   has_many :users, :through => :user_courses
   has_many :user_courses
   belongs_to :term
-  set_table_name "course"
-  set_primary_key "course_id"
+  self.table_name = "course"
+  self.primary_key = "course_id"
 end
--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,5 +1,5 @@
 class UserCourse < ActiveRecord::Base
-  set_table_name "user_course"
+  self.table_name = "user_course"
   belongs_to :user
   belongs_to :course
 end
