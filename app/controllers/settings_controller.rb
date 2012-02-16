0bd6659adefb5e10f44fe9dd4a765a7da47de6ee
Al Wold <alwold@gmail.com>
2012-02-15 20:48:30 -0700

    limited updating of user data

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,7 +3,15 @@ require 'java'
 class SettingsController < ApplicationController
   before_filter :authenticate_user!
   def index
-    @user = User.where("email = ?", current_user.email).first
+    @user = User.find(current_user.id)
     @notifiers = SPRING_CONTEXT.getBeansOfType(com.alwold.classwatch.notification.Notifier.java_class).values
+  end
+
+  def update
+    user = User.where("email = ?", current_user.email).first
+    user.email = params[:user][:email]
+    user.phone = params[:user][:phone]
+    user.save
+    redirect_to :action => :index
   end
 end
