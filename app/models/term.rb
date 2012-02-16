fc5131f8266f1fbd7fe37122b3b64e42702616f9
Al Wold <alwold@gmail.com>
2012-02-15 17:41:10 -0700

    use un-deprecated pk/table name setup

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,8 +1,8 @@
 require 'date'
 
 class Term < ActiveRecord::Base
-  set_table_name "term"
-  set_primary_key "term_id"
+  self.table_name = :term
+  self.primary_key = :term_id
   def Term.get_active_terms
     Term.where("start_date <= :now and end_date >= :now", { :now => Date.today })
   end
