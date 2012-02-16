4dcf8b09c4d4f577aac6752d787483962c4d324f
Al Wold <alwold@gmail.com>
2012-02-15 17:42:27 -0700

    these were copied here by mistake...goodbye

--- models/course.rb
+++
@@ -1,5 +0,0 @@
-class Course < ActiveRecord::Base
-  has_and_belongs_to_many :users, { :join_table => 'user_course' }
-  set_table_name "course"
-  set_primary_key "course_id"
-end
--- models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  has_and_belongs_to_many :courses, { :join_table => "user_course" }
-  set_primary_key "user_id"
-end
