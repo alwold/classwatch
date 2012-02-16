41cee59f7c6ce54a0070822b4840f97f68b9d341
Al Wold <alwold@gmail.com>
2012-02-15 17:03:42 -0700

    only fire up spring if we are in jruby

--- config/initializers/01_java_jars.rb
+++ config/initializers/01_java_jars.rb
@@ -1,5 +1,7 @@
-Dir.entries("#{Rails.root}/lib").sort.each do |entry|
-  if entry =~ /.jar$/
-    require entry
+if RUBY_PLATFORM == "java"
+  Dir.entries("#{Rails.root}/lib").sort.each do |entry|
+    if entry =~ /.jar$/
+      require entry
+    end
   end
 end
--- config/initializers/02_spring.rb
+++ config/initializers/02_spring.rb
@@ -1,8 +1,10 @@
-SPRING_XML_CONFIG_FILES = %w(
-     config/applicationContext
-     classwatchDao).map { |c|
-   "classpath:#{c}.xml" 
- }.to_java :string
+if RUBY_PLATFORM == "java" then
+  SPRING_XML_CONFIG_FILES = %w(
+       config/applicationContext
+       classwatchDao).map { |c|
+     "classpath:#{c}.xml" 
+   }.to_java :string
  
- SPRING_CONTEXT = org.springframework.context.support.
-   ClassPathXmlApplicationContext.new(SPRING_XML_CONFIG_FILES)
+   SPRING_CONTEXT = org.springframework.context.support.
+     ClassPathXmlApplicationContext.new(SPRING_XML_CONFIG_FILES)
+end
