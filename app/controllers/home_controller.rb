429700d857e521af35897743a00efc2ef50d04e2
Al Wold <alwold@gmail.com>
2012-02-15 17:42:59 -0700

    add class name lookup functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
+    mgr = SPRING_CONTEXT.getBean("classInfoManager")
     @user = User.where("email = ?", current_user.email).first
     @courses = Course.joins(:user_courses => :user).where(:users => {:user_id => @user})
+    @class_infos = Array.new
+    @courses.each do |course|
+      @class_infos.push(mgr.getClassInfo(1, course.term.term_code, course.course_number))
+    end
     @terms = Term.get_active_terms.map { |term| [term.name, term.id] }
     @notifications = Array.new
   end
