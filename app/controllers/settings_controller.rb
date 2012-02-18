c1492859b8b46e08d39957b9d5229ff7dee6f07f
Al Wold <alwold@gmail.com>
2012-02-17 17:22:13 -0700

    password changes work now

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,10 +8,30 @@ class SettingsController < ApplicationController
   end
 
   def update
-    user = User.where("email = ?", current_user.email).first
+    logger.debug "update"
+    user = User.find(current_user.id)
     user.email = params[:user][:email]
     user.phone = params[:user][:phone]
-    user.save
+    if (params[:old_password] != nil && !params[:old_password].empty?) || (params[:password] != nil && !params[:password].empty?) || (params[:confirm_password] != nil && !params[:confirm_password].empty?) then
+      logger.debug "doing a password change"
+      if user.valid_password? params[:old_password] then
+        logger.debug "password is valid"
+        if params[:password].empty? then
+          flash[:error] = "Please enter a new password"
+        elsif params[:password] == params[:confirm_password] then
+          logger.debug "setting password to "+params[:password]
+          user.password = params[:password]
+          sign_in user, :bypass => true
+        else
+          flash[:error] = "Passwords do not match"
+        end
+      else
+        logger.debug "old password incorrect"
+        flash[:error] = "Old Password is incorrect"
+      end
+    end
+
+    user.save!
     redirect_to :action => :index
   end
 end
