10a300a1cbd255daf8b600e06338910a4c7bbb5d
Al Wold <alwold@gmail.com>
2012-02-27 11:48:43 -0700

    add validation on phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :phone, :numericality => true, :length => { :is => 10 }, :if => "!phone.nil?"
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :user_courses
