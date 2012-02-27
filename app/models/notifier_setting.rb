409e346ba4713f38a30e0fbe0a786cdbc2b13bf5
Al Wold <alwold@gmail.com>
2012-02-27 16:08:03 -0700

    add notifier setting model

---
+++ app/models/notifier_setting.rb
@@ -0,0 +1,7 @@
+class NotifierSetting < ActiveRecord::Base
+  self.table_name = :notifier_setting
+  self.primary_key = :notifier_setting_id
+  self.inheritance_column = :inheritance_type
+
+  belongs_to :user
+end
