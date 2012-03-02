e59bc152c8af2998568e222138d667c1133b922a
Al Wold <alwold@gmail.com>
2012-03-02 16:46:44 -0700

    make notifications show up properly

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,7 @@ class Notification < ActiveRecord::Base
   self.table_name = :notification
   self.primary_key = :notfication_id
   self.inheritance_column = :inheritance_type
+
+  belongs_to :user
+  belongs_to :course
 end
