6631866d941bab0533adaab3a0e24e5f28f9d43b
Al Wold <alwold@gmail.com>
2012-03-06 18:42:08 -0700

    use the class method to add classes

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -2,29 +2,8 @@ class ClassesController < ApplicationController
   before_filter :authenticate_user!
   def create
     user = User.where("email = ?", current_user.email).first
-    # first see if there is an existing course
-    course = Course.where("term_id = ? and course_number = ?", params[:course][:term_id], params[:course][:course_number]).first
-    if course == nil then
-      mgr = SPRING_CONTEXT.getBean("classInfoManager")
-      term = Term.find(params[:course][:term_id])
-      # TODO un-hard-code the institution id below
-      status = mgr.get_class_status 1, term.term_code, params[:course][:course_number]
-      if status == com.alwold.classwatch.model.Status::OPEN then
-        course = Course.new(params[:course])
-        course.save
-        # this is here temporarily because of a jruby postgres bug
-        course = Course.where("term_id = ? and course_number = ?", params[:course][:term_id], params[:course][:course_number]).first
-      end
-    end
-    if course != nil then
-      user_course = UserCourse.new
-      user_course.user = user
-      user_course.course = course
-      user_course.notified = false
-      user_course.save
-    else
-      flash[:error] = "Course was not found"
-    end
+    error = Course.add user, params[:course][:term_id], params[:course][:course_number]
+    flash[:error] = error if error
     redirect_to :root
   end
 
