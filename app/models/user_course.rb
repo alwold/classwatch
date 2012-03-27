3868a9c5d1b73ca4a1b59e16e50e76a893b510bb
Al Wold <alwold@gmail.com>
2012-03-27 00:28:26 -0700

    add cascading on delete of user_course

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -2,5 +2,5 @@ class UserCourse < ActiveRecord::Base
   self.table_name = "user_course"
   belongs_to :user
   belongs_to :course
-  has_many :notifier_settings
+  has_many :notifier_settings, :dependent => :delete_all
 end
