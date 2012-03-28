d38896505f0a470376c78487eb1532bb58cac456
Al Wold <alwold@gmail.com>
2012-03-28 15:14:55 -0700

    set up smtp settings to point to my local smtp server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Classwatch::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -35,4 +35,8 @@ Classwatch::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.cox.net"
+  }
 end
