e3cee42d0ce8772c8318a30f08f92816f7198759
Al Wold <alwold@gmail.com>
2012-03-27 17:29:55 -0700

    map school table

---
+++ app/models/school.rb
@@ -0,0 +1,4 @@
+class School < ActiveRecord::Base
+  set_table_name :school
+  set_primary_key :school_id
+end
--- app/models/term.rb
+++ app/models/term.rb
@@ -3,6 +3,9 @@ require 'date'
 class Term < ActiveRecord::Base
   self.table_name = :term
   self.primary_key = :term_id
+
+  belongs_to :school
+
   def Term.get_active_terms
     Term.where("start_date <= :now and end_date >= :now", { :now => Date.today })
   end
