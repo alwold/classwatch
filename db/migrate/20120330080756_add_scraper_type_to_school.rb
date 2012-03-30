b29615b815b325e352cb6b54cc5d97272d03a48a
Al Wold <alwold@gmail.com>
2012-03-30 01:59:39 -0700

    add scraper type column to school def

---
+++ db/migrate/20120330080756_add_scraper_type_to_school.rb
@@ -0,0 +1,17 @@
+class AddScraperTypeToSchool < ActiveRecord::Migration
+  def self.up
+    change_table :school do |t|
+      t.string :scraper_type, limit: 15
+    end
+    s = School.where(:name => "Arizona State University").first
+    s.scraper_type = "ASU"
+    s.save
+    change_column :school, :scraper_type, :string, null: false
+  end
+
+  def self.down
+    change_table :school do |t|
+      t.remove :scraper_type
+    end
+  end
+end
