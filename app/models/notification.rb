74a205538a04e88673581ea14cac69b46f5442a7
Al Wold <alwold@gmail.com>
2012-03-30 01:58:44 -0700

    fix typo in pk

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ActiveRecord::Base
   self.table_name = :notification
-  self.primary_key = :notfication_id
+  self.primary_key = :notification_id
   self.inheritance_column = :inheritance_type
 
   belongs_to :user
