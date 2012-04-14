7c983ea49a8e7825bede94fbc64512bdc125400e
Al Wold <alwold@gmail.com>
2012-04-14 00:08:35 -0700

    CW-17 convert blank phone to nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :phone, :numericality => true, :length => { :is => 10 }, :if => "!phone.empty?"
+  validates :phone, :numericality => true, :length => { :is => 10 }, :if => "!phone.nil?"
   before_validation :clean_phone
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :phone
@@ -12,8 +12,11 @@ class User < ActiveRecord::Base
   self.primary_key = :user_id
 
   def clean_phone
-    logger.debug("phone before #{self.phone.class} #{self.phone}")
-    self.phone = self.phone.gsub('-', '') if !self.phone.nil?
-    logger.debug("phone after #{self.phone.class} #{self.phone}")
+    # TODO figure out why self.phone works, but phone doesn't
+    if self.phone.blank?
+      self.phone = nil
+    else
+      self.phone = self.phone.gsub('-', '')
+    end
   end
 end
