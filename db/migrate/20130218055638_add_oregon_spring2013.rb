70ea04121af7bb5ecec56ff21dd0bde547fdcc0c
Al Wold <alwold@gmail.com>
2013-02-17 23:18:19 -0700

    add terms for oregon

---
+++ db/migrate/20130218055638_add_oregon_spring2013.rb
@@ -0,0 +1,17 @@
+class AddOregonSpring2013 < ActiveRecord::Migration
+  def up
+    school = School.where(:name => "University of Oregon").first
+    term = Term.new
+    term.term_code = "201203"
+    term.name = "Spring 2013"
+    term.start_date = Date.new(2013, 2, 8)
+    term.end_date = Date.new(2013, 6, 10)
+    term.school = school
+    term.save!
+  end
+
+  def down
+    school = School.where(:name => "University of Oregon").first
+    Term.where(:school_id => school, :term_code => "201203").delete_all
+  end
+end
