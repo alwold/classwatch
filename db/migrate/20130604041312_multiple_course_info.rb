c06d4f8e061180cdd64e97de8b4f3f14aaab446d
Al Wold <alwold@alwold.com>
2013-06-04 13:47:38 -0700

    add database support for multiple courses

--- db/migrate/20130604041312_multiple_course_info.rb
+++ db/migrate/20130604041312_multiple_course_info.rb
@@ -3,9 +3,15 @@ class MultipleCourseInfo < ActiveRecord::Migration
     rename_column :school, :course_number_name, :input_1_name
     add_column :school, :input_2_name, :string
     add_column :school, :input_3_name, :string
+    rename_column :course, :course_number, :input_1
+    add_column :course, :input_2, :string
+    add_column :course, :input_3, :string
   end
 
   def down
+    remove_column :course, :input_3
+    remove_column :course, :input_2
+    rename_column :course, :input_1, :course_number
     remove_column :school, :input_3_name
     remove_column :school, :input_2_name
     rename_column :school, :input_1_name, :course_number_name
