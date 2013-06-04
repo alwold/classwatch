d800fbc90f1c579096f9185cb5fffd4c3a1865c7
Al Wold <alwold@alwold.com>
2013-06-04 15:20:35 -0700

    use all parts of course info in email message

--- app/mailers/class_open_mailer.rb
+++ app/mailers/class_open_mailer.rb
@@ -3,7 +3,7 @@ class ClassOpenMailer < ActionMailer::Base
 
   def class_open_email(user, course, class_info)
     @school = course.term.school.name
-    @course_number = course.course_number
+    @course_number = "#{course.input_1} #{course.input_2} #{course.input_3}"
     @class_info = class_info
     mail(:to => user.email, :subject => "Your class, #{@class_info.name} (#{@course_number}), is now available!")
   end
