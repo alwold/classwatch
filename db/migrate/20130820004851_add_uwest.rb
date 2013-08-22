60be36ea035985ea98afbe352b4dbaebfeae49f0
Al Wold <alwold@alwold.com>
2013-08-21 19:43:56 -0700

    add uwest info

---
+++ db/migrate/20130820004851_add_uwest.rb
@@ -0,0 +1,27 @@
+class AddUwest < ActiveRecord::Migration
+  def up
+    School.reset_column_information
+    Term.reset_column_information
+    s = School.new
+    s.name = "University of the West"
+    s.scraper_type = "UWEST"
+    s.schedule_link = "http://uwest.edu/"
+    s.help_file = "uwest"
+    s.input_1_name = "Course"
+    s.save!
+
+    t = Term.new
+    t.term_code = "162"
+    t.name = "Fall 2013"
+    t.start_date = Date.new(2013, 8, 9)
+    t.end_date = Date.new(2014, 5, 17)
+    t.school = s
+    t.save!
+  end
+
+  def down
+    s = School.where(:name => "University of the West").first
+    Term.where(:school_id => s).delete_all
+    s.destroy
+  end
+end
