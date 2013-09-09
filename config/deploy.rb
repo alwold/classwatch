045d94477eb40e0ea321f194268a9f90b0dfe827
Al Wold <alwold@alwold.com>
2013-09-09 15:50:05 -0700

    awesome up the capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,34 +3,21 @@ require 'bundler/capistrano'
 set :application, "classwatch"
 set :repository,  "git://github.com/alwold/classwatch.git"
 
-#set :rails_env, :development
-set :bundle_without, [:test]
-#set :rake, "#{rake} --trace"
-
 set :scm, :git
 
 set :deploy_to, "/home/alwold/classwatch"
 set :use_sudo, false
 
-role :web, "alwold.com"                          # Your HTTP server, Apache/etc
-role :app, "alwold.com"                          # This may be the same as your `Web` server
-role :db,  "alwold.com", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+role :web, "alwold.com"
+role :app, "alwold.com"
+role :db,  "alwold.com", :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-default_run_options[:pty] = true
-
-#after "deploy:update_code","deploy:config_symlink"
 before "deploy:assets:precompile", "deploy:symlink_db"
 
-after "deploy:update", "deploy:restart"
+before "deploy:restart", "deploy:migrate"
+after "deploy:restart", "deploy:restart_daemon"
 
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
@@ -38,5 +25,10 @@ namespace :deploy do
       run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
       run "ln -nfs #{deploy_to}/shared/config/twilio.yml #{release_path}/config/twilio.yml"
       run "ln -nfs #{deploy_to}/shared/config/stripe.yml #{release_path}/config/stripe.yml"
+   end
+   task :restart_daemon, :roles => :app do
+      migrate_env = fetch(:migrate_env, "production")
+      run "cd #{release_path} && RAILS_ENV=#{migrate_env} bundle exec lib/daemons/classwatch_daemon_ctl stop"
+      run "cd #{release_path} && RAILS_ENV=#{migrate_env} bundle exec lib/daemons/classwatch_daemon_ctl start"
    end
 end
