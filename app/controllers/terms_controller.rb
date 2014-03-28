dc1ae9904e388beee1e2d4b76574cd89981fd1f6
Al Wold <alwold@alwold.com>
2014-03-28 12:20:19 -0700

    add support for adding terms

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,10 +1,21 @@
 class TermsController < ApplicationController
   before_filter :authenticate_user!, except: :get
   load_and_authorize_resource except: :get
-  
+
   def get
     terms = Term.get_active_terms(params[:school_id])
     school = School.find(params[:school_id])
     render :json => { terms: terms, school: school }
+  end
+
+  def index
+    @terms = Term.order('school_id, term_code')
+  end
+
+  def create
+    term = Term.new(params[:term])
+    term.save!
+
+    redirect_to terms_path
   end
 end
