0b989a6b4510ed4217b7517503ddad8e97e331b4
Al Wold <alwold@alwold.com>
2014-06-27 10:58:19 -0700

    add tests for logged in case

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'capybara'
+  gem 'poltergeist'
 end
 
 gem 'devise'
--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,17 @@ FactoryGirl.define do
     disable_adding false
     disable_watching false
   end
+
+  factory :user do
+    email 'johndoe@gmail.com'
+    password 'abc123'
+  end
+
+  factory :term do
+    term_code '2147'
+    name 'Fall 2014'
+    start_date Date.new(2014, 6, 1)
+    end_date Date.new(2014, 11, 1)
+    school
+  end
 end
