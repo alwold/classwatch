c93bbd4e67be75b334232aaa3541aec0f4ceb176
Al Wold <alwold@alwold.com>
2014-09-09 23:17:26 -0700

    add test for caching

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Course do
+  describe '#info_cache_key' do
+    it 'computes correct cache key'
+  end
   describe '#get_class_info' do
+    before :each do
+      Rails.cache.clear
+    end
     it 'gets info for a class' do
       course = create(:course)
       class_info = course.get_class_info
@@ -12,7 +18,12 @@ describe Course do
       course = create(:invalid_course)
       expect(course.get_class_info).to be_nil
     end
-    it "caches class info"
+    it "caches class info" do
+      course = create(:course)
+      expect(Rails.cache.exist? course.info_cache_key).to be_false
+      course.get_class_info
+      expect(Rails.cache.exist? course.info_cache_key).to be_true
+    end
   end
 
   describe "#get_class_status" do
