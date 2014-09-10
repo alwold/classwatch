fe3f1cc6cc9f27a54b5ec680d8933bc1aedc779a
Al Wold <alwold@alwold.com>
2014-09-10 14:52:08 -0700

    add scenario where user signs up during adding class

--- spec/features/class_spec.rb
+++ spec/features/class_spec.rb
@@ -51,8 +51,39 @@ feature 'class management', js: true do
     click_button 'Submit Payment'
     expect(page).to have_content('Your card number is incorrect.')
     expect(current_path).to eq pay_classes_path
-    page.save_screenshot 'screenshot.png'
   end
-  scenario 'new user adds class and signs up in process'
+
+  scenario 'new user adds class and signs up in process' do
+    term = FactoryGirl.create(:term)
+
+    visit root_path
+    select term.school.name, from: 'School'
+    find_field 'Term'
+    select term.name, from: 'Term'
+    fill_in 'Course Number', with: '12345'
+    click_button 'Add'
+
+    # create acct
+    expect(current_path).to eq new_user_session_path
+    click_link 'Sign up'
+
+    expect(current_path).to eq new_user_registration_path
+
+    fill_in 'Email', with: 'johndoe@johndoe.com'
+    fill_in 'Password', with: 'abc123'
+    fill_in 'Password confirmation', with: 'abc123'
+    click_button 'Sign up'
+
+    expect(current_path).to eq create_class_from_session_path
+    fill_in 'Card Number', with: '4242424242424242'
+    fill_in 'CVC', with: '123'
+    fill_in 'exp-month', with: '12'
+    fill_in 'exp-year', with: '2016'
+    click_button 'Submit Payment'
+
+    expect(page).to have_content('You are watching')
+    expect(page).to have_content('Fake Course')
+    expect(current_path).to eq root_path
+  end
   scenario 'existing user adds class and logs in in process'
 end
