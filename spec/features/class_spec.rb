9c4faafcb080aa7df797768093041790f54e75b7
Al Wold <alwold@alwold.com>
2014-09-10 14:55:30 -0700

    add test where existing user signs in in the process

--- spec/features/class_spec.rb
+++ spec/features/class_spec.rb
@@ -57,6 +57,7 @@ feature 'class management', js: true do
     term = FactoryGirl.create(:term)
 
     visit root_path
+    expect(page).to have_content('To get started')
     select term.school.name, from: 'School'
     find_field 'Term'
     select term.name, from: 'Term'
@@ -85,5 +86,35 @@ feature 'class management', js: true do
     expect(page).to have_content('Fake Course')
     expect(current_path).to eq root_path
   end
-  scenario 'existing user adds class and logs in in process'
+
+  scenario 'existing user adds class and logs in in process' do
+    user = FactoryGirl.create(:user)
+    term = FactoryGirl.create(:term)
+
+    visit root_path
+    expect(page).to have_content('To get started')
+
+    select term.school.name, from: 'School'
+    find_field 'Term'
+    select term.name, from: 'Term'
+    fill_in 'Course Number', with: '12345'
+    click_button 'Add'
+
+    expect(current_path).to eq new_user_session_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'abc123'
+    click_button 'Sign in'
+
+    expect(current_path).to eq create_class_from_session_path
+    fill_in 'Card Number', with: '4242424242424242'
+    fill_in 'CVC', with: '123'
+    fill_in 'exp-month', with: '12'
+    fill_in 'exp-year', with: '2016'
+    click_button 'Submit Payment'
+
+    expect(page).to have_content('You are watching')
+    expect(page).to have_content('Fake Course')
+    expect(current_path).to eq root_path  
+  end
 end
